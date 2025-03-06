c501a6cdd0e23882102a43a06f85defabd8b9a94
khushboo-590 <khushbook4979@gmail.com>
2025-03-06 19:43:22 +0530

    page done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,15 @@
-const navbarbtn = () => {
-    document.getElementById("name").classList.toggle("show-navbar")
-    document.getElementById("rotates").classList.toggle("rotate-42")
-    document.getElementById("transparent").classList.toggle("bg-transparent")
-    document.getElementById("rotates-2").classList.toggle("rotate-43")
-    document.body.classList.toggle("overflow-hidden")
-}
+// const navbarbtn = () => {
+//     document.getElementById("name").classList.toggle("show-navbar")
+//     document.getElementById("rotates").classList.toggle("rotate-42")
+//     document.getElementById("transparent").classList.toggle("bg-transparent")
+//     document.getElementById("rotates-2").classList.toggle("rotate-43")
+//     document.body.classList.toggle("overflow-hidden")
+// }
+
+let menuIcon = document.querySelector(".menu-icon");
+let menuList = document.querySelector(".menu-list");
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("show")
+    let activeNav = menuList.hasAttribute("class", "show");
+    if (activeNav) { menuIcon.classList.toggle("active") };
+})
