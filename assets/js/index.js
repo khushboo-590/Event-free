1f228c2b2125cc41867b22d253e46d2296915e17
khushboo-590 <khushbook4979@gmail.com>
2025-03-07 08:58:56 +0530

    footer-done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,4 @@
-// const navbarbtn = () => {
-//     document.getElementById("name").classList.toggle("show-navbar")
-//     document.getElementById("rotates").classList.toggle("rotate-42")
-//     document.getElementById("transparent").classList.toggle("bg-transparent")
-//     document.getElementById("rotates-2").classList.toggle("rotate-43")
-//     document.body.classList.toggle("overflow-hidden")
-// }
+
 
 let menuIcon = document.querySelector(".menu-icon");
 let menuList = document.querySelector(".menu-list");
